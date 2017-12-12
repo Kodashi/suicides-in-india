ac4afbc596bd8a90b3ebf30b8624658a7a4e9187
MKabMan <60070060@kmitl.ac.th>
2017-12-12 23:56:28 +0700

    Add files via upload
    new py

---
+++ new graph py/2010 Dead each Town.py
@@ -0,0 +1,29 @@
+"""Process Data How many people suicides each Town in [2010]"""
+import json
+import pygal
+with open('data_suicide.json') as test_data: #receive data
+
+    data = json.load(test_data)
+    city = {}
+    remove_case = ["Total (All India)", "Total (States)", "Total (Uts)"]
+
+    for i in data:
+        """edit year to find 2010 and check requestment"""
+        if  i["State"] not in remove_case and i["Age_group"] != "0-100+" and i["Year"] == 2010:
+            """check in have it dict already or not"""
+            if i["State"] in city:
+                city[i["State"]] += i["Total"]
+
+            else:
+                city[i["State"]] = i["Total"]
+
+    print("***Complete***")    #finish
+
+    """show data process"""
+    list_city = sorted(list(city))
+    line_chart = pygal.HorizontalBar(height=800, width=1000)
+    line_chart.title = "2010"
+    for i in list_city:
+        line_chart.add(i, city[i])
+        print("%s : %d" %(i, city[i]))
+    line_chart.render_to_file('2010.svg')
---
+++ new graph py/2012 Dead each Town.py
@@ -0,0 +1,29 @@
+"""Process Data How many people suicides each Town in [2012]"""
+import json
+import pygal
+with open('data_suicide.json') as test_data: #receive data
+
+    data = json.load(test_data)
+    city = {}
+    remove_case = ["Total (All India)", "Total (States)", "Total (Uts)"]
+
+    for i in data:
+        """edit year to find 2012 and check requestment"""
+        if  i["State"] not in remove_case and i["Age_group"] != "0-100+" and i["Year"] == 2012:
+            """check in have it dict already or not"""
+            if i["State"] in city:
+                city[i["State"]] += i["Total"]
+
+            else:
+                city[i["State"]] = i["Total"]
+
+    print("***Complete***")    #finish
+
+    """show data process"""
+    list_city = sorted(list(city))
+    line_chart = pygal.HorizontalBar(height=800, width=1000)
+    line_chart.title = "2012"
+    for i in list_city:
+        line_chart.add(i, city[i])
+        print("%s : %d" %(i, city[i]))
+    line_chart.render_to_file('2012.svg')
